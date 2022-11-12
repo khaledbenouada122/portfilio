52efa1bd2e53e9cbd778a1ab86dc80f52c530c63
Nader Elhaj <naderelhaj3@gmail.com>
2022-11-12 13:42:08 +0100

    fixing Contant margins and paddings

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -16,30 +16,30 @@ function Contact() {
   };
   return (
     <div className="  h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center pb-20  ">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
         CONTACT
       </h3>
-      <div className="flex flex-col space-y-10 mt-64 xl:mt-44">
-        <h4 className="text-4xl font-semibold text-center">
+      <div className="flex flex-col space-y-6 mt-60 lg:mt-28">
+        <h4 className="text-2xl md:text-4xl font-semibold text-center w-screen mx-auto">
           I have got what you need,
           <span className="decoration-[#f7ab0a] underline">Lets Talk.</span>
         </h4>
-        <div className="space-y-10">
+        <div className="space-y-4">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="h-7 w-7 text-[#f7ab0a] " />
-            <p className="text-2xl">+216 52 241 103</p>
+            <p className="text-xl md:2xl">+216 52 241 103</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="h-7 w-7 text-[#f7ab0a] " />
-            <p className="text-2xl">naderelhaj3@gmail.com</p>
+            <p className="text-xl md:2xl">naderelhaj3@gmail.com</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <MapIcon className="h-7 w-7 text-[#f7ab0a] " />
-            <p className="text-2xl">Sahloul , Sousse</p>
+            <p className="text-xl md:2xl">Sahloul , Sousse</p>
           </div>
         </div>
         <form
-          className="flex flex-col space-y-2 w-fit mx-auto"
+          className="flex flex-col space-y-2 w-fit mx-auto" 
           onSubmit={handleSubmit(onSubmit)}
         >
           <div className="flex space-x-2">
